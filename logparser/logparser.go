a93a30216e0f38e003b924abee615033a51489c2
Jan Halfar <jan@bestbytes.com>
2016-04-19 11:08:41 +0200

    parsing progress

--- logparser/logparser.go
+++ logparser/logparser.go
@@ -3,8 +3,120 @@ package logparser
 import (
 	"errors"
 	"fmt"
-	"log"
+	"strconv"
+	"strings"
+	"time"
 )
+
+type Level int
+
+const (
+	LevelEError            Level = 1
+	LevelEWarning                = 2
+	LevelEParse                  = 4
+	LevelENotice                 = 8
+	LevelECoreError              = 16
+	LevelECoreWarning            = 32
+	LevelECompileError           = 64
+	LevelECompileWarning         = 128
+	LevelEUserError              = 256
+	LevelEUserWarning            = 512
+	LevelEUserNotice             = 1024
+	LevelEStrict                 = 2048
+	LevelERecoverableError       = 4096
+	LevelEDeprecated             = 8192
+	LevelEUserDeprecated         = 16384
+)
+
+type LogLine struct {
+	Level Level
+	Time  time.Time
+	Error string
+	Line  int
+}
+
+// LogParser parses logs
+type Parser struct {
+	filename  string
+	locations map[string]*time.Location
+}
+
+// NewLogParser well a fresh log parser, if your log file is not too fishy
+func NewLogParser(filename string) (p *Parser, err error) {
+	return &Parser{
+		filename:  filename,
+		locations: map[string]*time.Location{},
+	}, nil
+}
+
+func getFileAndLine(line string) (filename string, lineNumber int, err error) {
+	// [02-Dec-2015 14:35:00 Europe/Berlin] PHP Fatal error:  hello in /var/path with blanks/to in depp.php on line 3
+	err = errors.New("no found")
+	lastIn := strings.LastIndex(line, " in /")
+	if lastIn == -1 {
+		return
+	}
+
+	rest := line[lastIn+4:] // 4 == len(" in ")
+
+	words := strings.Split(rest, " ")
+	if len(words) < 4 {
+		return
+	}
+	numWords := len(words)
+	if words[numWords-2] != "line" || words[numWords-3] != "on" {
+		return
+	}
+
+	lineNumberString := words[numWords-1]
+	lineNumber, err = strconv.Atoi(lineNumberString)
+	if err != nil {
+		err = errors.New("could not parse lineNumber : " + err.Error())
+		return
+	}
+	filename = strings.Join(words[:numWords-3], " ")
+
+	if len(filename) == 0 {
+		err = errors.New("filename not found")
+		return
+	}
+	err = nil
+
+	return
+}
+
+func (p *Parser) getDate(line string) (t time.Time, err error) {
+	dateString, loc, err := extractDateStringFromLine(line)
+	if err != nil {
+		return
+	}
+	location, ok := p.locations[loc]
+	if !ok {
+		location, err = time.LoadLocation(loc)
+		if err != nil {
+			return
+		}
+		p.locations[loc] = location
+	}
+	return getDate(dateString, location)
+}
+
+func getDate(dateString string, location *time.Location) (date time.Time, err error) {
+	return time.ParseInLocation(timeLayout, dateString, location)
+}
+
+func extractErrorNameFromLine(line string) (errorName string, err error) {
+	start := strings.Index(line, "] PHP ")
+	if start == -1 {
+		return "", errors.New("start not found")
+	}
+	rest := line[start+6:] // 6 len("] PHP ")
+	end := strings.Index(rest, ":")
+	if end == -1 {
+		return "", errors.New("end not found")
+	}
+	return rest[:end], nil
+}
 
 // Mon Jan 2 15:04:05 MST 2006 (MST is GMT-0700)
 //const timeLayout = "02-Jan-2006 15:04:05 America/Denver"
@@ -16,7 +128,7 @@ func extractDateStringFromLine(line string) (dateString string, location string,
 	if len(line) > 30 {
 		part := 0
 		for index, char := range line {
-			log.Println(index, char, string(char))
+			//log.Println(index, char, string(char))
 			switch index {
 			case 0:
 				if char != '[' {
@@ -30,12 +142,28 @@ func extractDateStringFromLine(line string) (dateString string, location string,
 			}
 			switch char {
 			case ' ':
+				if part == 0 {
+					dateString += " "
+				}
 				part++
+				if part > 2 {
+					return
+				}
 				continue
 			case ']':
 				err = nil
+				minLength := index + 4 // 4 == len(" PHP")
+				if len(line) < minLength {
+					err = errors.New("missing ] PHP")
+					return
+				}
+				if line[index:(index+5)] != "] PHP" {
+					err = errors.New("missing'] PHP '")
+					return
+				}
 				if len(dateString) != 20 && len(location) < 3 {
 					err = fmt.Errorf("invalid format %d %d", len(dateString), len(location))
+					return
 				}
 				return
 			}
--- logparser/logparser_test.go
+++ logparser/logparser_test.go
@@ -1,26 +1,115 @@
 package logparser
 
-import (
-	"testing"
-	"time"
-)
+import "testing"
 
 const (
-	lineNotice  = `[02-Dec-2015 14:32:23 Europe/Berlin] PHP Notice:  Foomo\Cache\Persistence\Queryable\PDOPersistor::connect PDO connected at attempt: 1 in /var/www/schild/modules/Foomo/lib/Foomo/Cache/Persistence/Queryable/PDOPersistor.php on line 386`
-	lineWarning = `[02-Dec-2015 14:35:00 Europe/Berlin] PHP Warning:  mkdir(): File exists in /var/www/schild/modules/Foomo/lib/Foomo/Setup.php on line 160`
+	lineCrap     = `böa`
+	lineLongCrap = `[¢jkjkljas fjksajfksajfklas jfklsjfklsaf jfklajkljafkljfkl asjklasjfklas jfkalsjfklsf jasklfj askl fjasklfjsklaf jaklfjkflas djklfasj fklasjdfkl j fklas jfakslöböa`
+	lineNotice   = `[02-Dec-2015 14:32:23 Europe/Berlin] PHP Notice:  Foomo\Cache\Persistence\Queryable\PDOPersistor::connect PDO connected at attempt: 1 in /var/www/schild/modules/Foomo/lib/Foomo/Cache/Persistence/Queryable/PDOPersistor.php on line 386`
+	lineWarning  = `[02-Dec-2015 14:35:00 Europe/Berlin] PHP Warning:  mkdir(): File exists in /var/www/schild/modules/Foomo/lib/Foomo/Setup.php on line 160`
+	lineAlmost   = `[02-Dec-2015 14:35:00 Europe/Berlin] PHp hjkh`
+
+	lineMultiUnknown = `[02-Dec-2015 14:35:00 Europe/Berlin] PHP Warning:  PHP Startup: Unable to load dynamic library '/usr/local/opt/php56-yaml/yaml.so' - dlopen(/usr/local/opt/php56-yaml/yaml.so, 9): Symbol not found: _basic_globals
+  Referenced from: /usr/local/opt/php56-yaml/yaml.so
+  Expected in: flat namespace
+ in /usr/local/opt/php56-yaml/yaml.so in Unknown on line 0`
+	lineInBlanks = `[02-Dec-2015 14:35:00 Europe/Berlin] PHP Fatal error:  hello in /Users/jan/go/src/github.com/foomo/phperrorlog_exporter/sepp in depp.php on line 3`
 )
 
-func TestDate(t *testing.T) {
-	location, _ := time.LoadLocation("Europe/Berlin")
-	date, err := time.ParseInLocation(timeLayout, "02-Dec-2015 14:32:23", location)
+func poe(err error) {
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
-	t.Log(date)
+}
+
+func pone(err error) {
+	if err == nil {
+		panic("no err")
+	}
+}
+
+func TestDate(t *testing.T) {
+	p, err := NewLogParser("foo")
+	poe(err)
+	lt, e := p.getDate(lineNotice)
+	t.Log(lt, e)
+	poe(e)
+	lt, e = p.getDate(lineWarning)
+	t.Log(lt, e)
+	poe(e)
+	lt, e = p.getDate(lineCrap)
+	t.Log(lt, e)
+	pone(e)
+	lt, e = p.getDate(lineLongCrap)
+	t.Log(lt, e)
+	pone(e)
+	lt, e = p.getDate(lineAlmost)
+	t.Log(lt, e)
+	pone(e)
+
+}
+
+func TestGetFileAndLine(t *testing.T) {
+	type expectedFileAndLine struct {
+		line int
+		file string
+		err  bool
+	}
+	expectations := map[string]expectedFileAndLine{
+		lineNotice: expectedFileAndLine{
+			line: 386,
+			file: "/var/www/schild/modules/Foomo/lib/Foomo/Cache/Persistence/Queryable/PDOPersistor.php",
+			err:  false,
+		},
+		lineCrap: expectedFileAndLine{
+			err: true,
+		},
+	}
+	for line, expectation := range expectations {
+		actualFile, actualLine, err := getFileAndLine(line)
+		t.Log("actualFile, actualLine, err", actualFile, actualLine, err, "from", line)
+		if expectation.err {
+			if err == nil {
+				t.Fatal("where is my error")
+			}
+			continue
+		}
+		if actualFile != expectation.file {
+			t.Fatal("file missmatch", actualFile, "!=", expectation.file)
+		}
+		if actualLine != expectation.line {
+			t.Fatal("line missmatch", actualLine, "!=", expectation.line)
+		}
+	}
+}
+
+func TestExtractErrorNameFromLine(t *testing.T) {
+	expectations := map[string]string{
+		lineNotice:   "Notice",
+		lineWarning:  "Warning",
+		lineAlmost:   "",
+		lineCrap:     "",
+		lineLongCrap: "",
+	}
+	for line, expectedName := range expectations {
+		errorName, err := extractErrorNameFromLine(line)
+		t.Log("expected", expectedName, "got", errorName, "in", line)
+		if expectedName == "" {
+			if err == nil || errorName != "" {
+				t.Fatal("there should have been an error", errorName)
+			}
+		} else {
+			if err != nil {
+				t.Fatal("unexpected error", err, "in", line)
+			}
+			if expectedName != errorName {
+				t.Fatal("unexpected", expectedName, "!=", errorName)
+			}
+		}
+	}
 }
 
 func TestExtractDateStringFromLine(t *testing.T) {
 	te, z, e := extractDateStringFromLine(lineNotice)
 	t.Log("te", te, "z", z, "e", e)
-
 }
