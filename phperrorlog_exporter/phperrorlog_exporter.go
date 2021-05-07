1e7718d8cd06c6fb846f089ba5608cc4a8657136
Daniele Scasciafratte <mte90net@gmail.com>
2021-05-07 16:22:19 +0200

    feat(reset): I don't like go but I think that now the metrics are resetted every time

--- phperrorlog_exporter/phperrorlog_exporter.go
+++ phperrorlog_exporter/phperrorlog_exporter.go
@@ -20,7 +20,6 @@ func main() {
 		fmt.Println("Usage:", os.Args[0], "domain:path/to/php/error.log [domain:path/to/other/log]")
 		flag.PrintDefaults()
 		os.Exit(1)
-
 	}
 
 	args := flag.Args()
@@ -28,12 +27,15 @@ func main() {
 	if len(args) < 1 {
 		usage()
 	}
+	files := 0
 	chanObservation := make(chan logparser.Observation)
 	for _, nameFile := range args {
 		parts := strings.Split(nameFile, ":")
 		if len(parts) != 2 {
 			usage()
 		}
+		files++
+		
 		go logparser.Observe(parts[0], parts[1], chanObservation, time.Second*10)
 	}
 
@@ -41,7 +43,7 @@ func main() {
 		prometheus.SummaryOpts{
 			Name: "php_errors",
 			Help: "How many PHP errors partitioned by type and site",
-            MaxAge: 100,
+            MaxAge: 1,
             AgeBuckets: 1,
 		},
 		[]string{"type", "site"},
@@ -56,13 +58,20 @@ func main() {
         fmt.Println("Listening on address:port => ", *flagHTTP)
 	}
 
+	run := 0
 	for {
 		select {
 		case observation := <-chanObservation:
+            if run == files {
+                run = 0
+                phpErrors.Reset()
+                fmt.Println("Reset done")
+            }
 			fmt.Println("observing", observation.Name, "stats", observation.Stats)
 			for t, v := range observation.Stats {
 				phpErrors.WithLabelValues(t, observation.Name).Observe(float64(v))
 			}
+            run++
 		}
 	}
 }
